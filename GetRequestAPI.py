f4583ef43c7561b72eed6dd8b593c4795ee7b24e
Brent C <cbrent0121@gmail.com>
2016-06-28 11:43:10 -0400

    Update GetRequestAPI.py

--- GetRequestAPI.py
+++ GetRequestAPI.py
@@ -5,7 +5,7 @@ def uber_get_price(start_lat, start_long, end_lat, end_long):
     url = 'https://api.uber.com/v1/estimates/price'
 
     parameters = {
-        'server_token': 'n6J7liODL-yg6YbmhdS2aAg35kYxQK4yUmhYc6_4',
+        'server_token': 'UBER_API_KEY',
         'start_latitude': start_lat,
         'start_longitude': start_long,
         'end_latitude': end_lat,
